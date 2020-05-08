c23bb61ae6eeb98fb71adb8d4467b520382242b4
glassbones <glassbonesofficial@gmail.com>
2020-05-08 12:59:32 -0500

    stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,53 @@
     <div class="right-button"> > </div>
   </div>
 */
+function carouselComp(entryPoint){
+
+  let imgs = ["./assets/carousel/mountains.jpeg","./assets/carousel/computer.jpeg","./assets/carousel/trees.jpeg","./assets/carousel/turntable.jpeg"]
+  let arr = []
+
+  arr.push(new Element('div','carousel',_ , entryPoint))
+  for (img of imgs) {arr.push(new Element('img',_,img))}
+  arr.push(new Element("div","left-button"))
+  arr.push(new Element("div","right-button"))
+
+  // if odd number move all right
+  if (arr.length % 2){
+    arr.forEach((el, index) => {
+      if (index > 1 && index < arr.length-2)
+      el.HTML.style.transform = `translateX(-150%)`;
+    });
+  } 
+  //right button
+  arr[arr.length -1].HTML.onclick =()=> {
+    arr[arr.length -3].HTML.style.display = 'none';
+    arr.forEach((el, index) => {
+      if (index > 0 && index < arr.length-2)
+      el.HTML.style.transform = `translateX(-100%)`;
+    });
+  };
+
+  //left button
+  arr[arr.length -2].HTML.onclick =()=> {
+    arr[1].HTML.style.display = 'none';
+    arr.forEach((el, index) => {
+      if (index > 0 && index < arr.length-2)
+      el.HTML.style.transform = `translateX(100%)`;
+    });
+  };
+
+
+  console.log(arr)
+  // raaaaaaaaaaaaaahhhhhhhhhhhhhh notttt enoughhhhh tiiiiiiiiiiiiiimmmmmmmmmmmmmmmmmmmeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    
+  
+  
+  arr[1].HTML.style.border = "solid red"
+  //arr[2].HTML.style.display = "none"
+  //arr[5].HTML.style.clipPath = "polygon(0 0, 50% 0, 48% 100%, 0% 100%);"
+  arr[arr.length-1].HTML.style.border = "solid red"
+  console.log(arr.length)
+}
+
+let carouselContainer = document.querySelector('div.carousel')
+carouselComp(carouselContainer)
